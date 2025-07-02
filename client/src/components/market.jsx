1a35d53ee9616704e20901ea338f33d872ba2130
3RaghavK3 <33raghavk33@gmail.com>
2025-07-03 02:57:52 +0530

    :changed broken api link

--- client/src/components/market.jsx
+++ client/src/components/market.jsx
@@ -18,7 +18,7 @@ export function Market() {
 
   useEffect(() => {
     // @ts-ignore
-    fetch(`${import.meta.env.VITE_API_URL}/market/?page=${page}&perPage=${perPage}`)
+    fetch(`${import.meta.env.VITE_API_URL}/market?page=${page}&perPage=${perPage}`)
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
