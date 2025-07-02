9f9e58963b957be0be3e2c640c5014536eb2c958
3RaghavK3 <33raghavk33@gmail.com>
2025-07-03 01:25:23 +0530

    :added nodefetch

--- server/fetchdata.js
+++ server/fetchdata.js
@@ -1,3 +1,5 @@
+import fetch from "node-fetch";
+
 export default async function fetchData(url) {
   const options = {
     method: 'GET',
--- server/index.js
+++ server/index.js
@@ -2,8 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import fetchData from './fetchdata.js'
 
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 
