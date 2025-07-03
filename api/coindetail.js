6c32ca7f3cf80398f68b7c3e84c1b6bc0bd43b6c
3RaghavK3 <33raghavk33@gmail.com>
2025-07-03 05:54:50 +0530

    :removed unnecessary client folder

--- api/coindetail.js
+++ api/coindetail.js
@@ -5,6 +5,7 @@ export default async function  handler(req,res) {
             try {
               const response = await fetch(`https://api.coingecko.com/api/v3/coins/${id}`);
               const data=await response.json()
+              console.log(data)
               res.status(200).json(data)
             } catch (err) {
               console.error('Backend Error: ', err.message);
