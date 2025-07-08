8db81cbbd05ea0217bd89148aeeb990531243f2d
3RaghavK3 <33raghavk33@gmail.com>
2025-07-08 23:14:10 +0530

    :Added localstorage for wishlist

--- src/context/wishlistcontext.jsx
+++ src/context/wishlistcontext.jsx
@@ -1,10 +1,19 @@
-import { createContext, useState } from 'react';
+import { createContext, use, useEffect, useState } from 'react';
 
 // @ts-ignore
 export const WishlistContext = createContext();
 
 export const WishlistProvider = ({ children }) => {
-  const [LikedCoins, setLikedCoins] = useState([]);
+
+  const [LikedCoins, setLikedCoins] = useState(()=>{
+     const stored=(localStorage.getItem('Wishlist')) 
+     if(stored) return JSON.parse(stored)
+     else return []
+  }
+  );
+   useEffect(() => {
+    localStorage.setItem('Wishlist', JSON.stringify(LikedCoins));
+  }, [LikedCoins]);
 
   return (
     <WishlistContext.Provider value={{ LikedCoins, setLikedCoins }}>
