e64f6acff448a7433ea22670e82650995b3f0b8e
3RaghavK3 <33raghavk33@gmail.com>
2025-11-13 13:13:25 +0530

    :env fix

--- server/index.js
+++ server/index.js
@@ -6,8 +6,8 @@ dotenv.config();
 
 import { Redis } from '@upstash/redis'
 const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_URL,
-  token: process.env.UPSTASH_REDIS_TOKEN,
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
 });
 
 const app = express();
