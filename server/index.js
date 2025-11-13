70f183367a587d7df2c5b52d3024bf31f031775a
3RaghavK3 <33raghavk33@gmail.com>
2025-11-13 13:01:35 +0530

    :feat:backend caching

--- server/index.js
+++ server/index.js
@@ -2,11 +2,17 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import fetch from 'node-fetch';
-
 dotenv.config();
+
+import { Redis } from '@upstash/redis'
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_URL,
+  token: process.env.UPSTASH_REDIS_TOKEN,
+});
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
 
 
 app.use(cors());
@@ -15,8 +21,14 @@ app.use(express.json());
 
 app.get('/api/global', async (req, res) => {
   try {
+    const cache_key="global_data"
+    const cached=await redis.get(cache_key)
+    if (cached) return res.json(cached)
+    
     const response = await fetch(`https://api.coingecko.com/api/v3/global`);
     const data = await response.json();
+    
+    await redis.set(cache_key,data,{ex:600})
     res.status(200).json(data);
   } catch (err) {
     console.error('Backend Error:', err.message);
@@ -27,11 +39,17 @@ app.get('/api/global', async (req, res) => {
 
 app.get('/api/market', async (req, res) => {
   const { page } = req.query;
+ 
   try {
+    const cache_key=`market_data_${page}`
+    const cached=await redis.get(cache_key)
+    if(cached) return res.json(cached)
+
     const response = await fetch(
       `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=100&page=${page}&sparkline=true&price_change_percentage=1h%2C24h%2C7d&precision=2`
     );
     const data = await response.json();
+    await redis.set(cache_key,data,{ex:300})
     res.status(200).json(data);
   } catch (err) {
     console.error('Backend Error:', err.message);
@@ -42,8 +60,12 @@ app.get('/api/market', async (req, res) => {
 
 app.get('/api/trending', async (req, res) => {
   try {
+  const cache_key="trending_data"
+  const cached=await redis.get(cache_key)
+  if(cached) return res.json(cached)
     const response = await fetch(`https://api.coingecko.com/api/v3/search/trending`);
     const data = await response.json();
+    await redis.set(cache_key,data,{ex:600});
     res.status(200).json(data);
   } catch (err) {
     console.error('Backend Error: ', err.message);
@@ -53,11 +75,15 @@ app.get('/api/trending', async (req, res) => {
 
 
 app.get('/api/coindetail', async (req, res) => {
-  const { id } = req.query;
+  const { id } = req.query; 
+
   try {
+    const cache_key=`coin_detail_${id}`
+    const cached=await redis.get(cache_key)
+    if(cached) return res.json(cached)
     const response = await fetch(`https://api.coingecko.com/api/v3/coins/${id}`);
     const data = await response.json();
-    console.log(data);
+    await redis.set(cache_key,data,{ex:120});
     res.status(200).json(data);
   } catch (err) {
     console.log('Backend Error: ', err.message);
